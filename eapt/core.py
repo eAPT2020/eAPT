653bec1c5d46dad8a4f7698413a1fa5bf3d28b91
TaeGeun Moon <xorms987@gmail.com>
2020-05-27 11:18:24 +0900

    add: requirements

--- eapt/core.py
+++ eapt/core.py
@@ -1,2 +1,10 @@
-def sayHi():
-    print('hi!')
+from urllib.request import urlopen
+import json
+import ssl
+
+def currentIpInfo():
+    info = None
+    with urlopen('http://free.ipwhois.io/json/') as response:
+        data = response.read().decode('utf-8')
+        info = json.loads(data)
+    return info
